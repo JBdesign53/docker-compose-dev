4fb7ee70ebd2f07a8b5fe525bafab8d96326004d
J.Busch <jeff.busch@melbourneit.com.au>
2019-04-27 14:01:24 +1000

    Use different PHP versions per virtual host

--- docker/www/php73/index.php
+++ docker/www/php73/index.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Docker: PHP-FPM Test</title>
+  <title>Docker: PHP-FPM 7.3 Test</title>
 </head>
 <body>
-  <h1>PHP-FPM Test Page</h1>
+  <h1>PHP-FPM 7.3 Test Page</h1>
 
   <?php phpinfo(); ?>
 </body>
